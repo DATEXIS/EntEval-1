8a3d956e2ca6a33ceb549aa15c4eba69090b9982
MrInformatic <maxundmoritz99199@gmail.com>
2020-05-14 12:49:39 +0200

    feat: adapt ERT tasks to score

--- enteval/ert.py
+++ enteval/ert.py
@@ -118,9 +118,9 @@ class ERTEval(object):
         config['classifier'] = config_classifier
 
         clf = SplitClassifier(self.X, self.y, config)
-        devacc, testacc = clf.run()
-        logging.debug('Dev acc : {0} Test acc : {1} for Entity Relationship\n'
-                      .format(devacc, testacc))
-        return {'devacc': devacc, 'acc': testacc,
+        dev_score, test_score = clf.run()
+        logging.debug('Dev Score : {0} Test Score : {1} for Entity Relationship\n'
+                      .format(dev_score, test_score))
+        return {'dev_score': dev_score, 'score': test_score,
                 'ndev': len(self.data['valid'][0]),
                 'ntest': len(self.data['test'][0])}
